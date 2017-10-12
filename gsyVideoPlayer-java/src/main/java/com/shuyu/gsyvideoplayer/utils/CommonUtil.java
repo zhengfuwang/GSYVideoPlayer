3c26af6a912b761427a6a82e303807281332c16c
xiyangyuge <xiyangyuge@gmail.com>
2017-10-12 12:23:53 +0800

    update sdk version

--- gsyVideoPlayer-java/src/main/java/com/shuyu/gsyvideoplayer/utils/CommonUtil.java
+++ gsyVideoPlayer-java/src/main/java/com/shuyu/gsyvideoplayer/utils/CommonUtil.java
@@ -9,12 +9,11 @@ import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.internal.view.ContextThemeWrapper;
+import android.support.v7.view.ContextThemeWrapper;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.Surface;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 
 import java.io.File;
